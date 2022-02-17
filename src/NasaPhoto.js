2439a2684fcad6db0b5ab67d69f24735ca1b7158
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-17 16:00:24 -0500

    added styled-components and got rid of some of the CSS in the css file

--- src/NasaPhoto.js
+++ src/NasaPhoto.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+//Regular
 
 function modal() {
   const modal = document.querySelector(".modal");
@@ -11,26 +14,82 @@ document.addEventListener("click", () => {
   remove.classList.remove("modal-active");
 });
 
+//Styled Components
+
+const ApodWrapperDiv = styled.div`
+  text-align: center;
+`;
+
+const HeaderDiv = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 0 20px;
+  padding-bottom: 35px;
+
+  background-color: ${(prop) => (prop.media === "image" ? "#fc3d21" : "#0b3d91")};
+
+  color: ${(prop) => (prop.media === "image" ? "#0b3d91" : "white")};
+`;
+
+const ApodTitleH1 = styled.h1`
+  padding-top: 20px;
+  font-weight: 700;
+  font-size: 3rem;
+`;
+
+const MainContentDiv = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  max-width: 1500px;
+`;
+
+const TextWrapperDiv = styled.div`
+  border: 3px solid ${(prop) => prop.theme.secondaryColor};
+  border-radius: 20px;
+  padding: 30px;
+  margin-bottom: 50px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`;
+
+const Button = styled.button`
+  font-family: "Poppins", sans-serif;
+  font-size: 1rem;
+  font-weight: 500;
+  border-radius: 30px;
+  padding: 10px;
+  margin-bottom: 50px;
+  transition: all 0.6s ease;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  border: 3px solid ${(prop) => prop.theme.secondaryColor};
+  background-color: ${(prop) => prop.theme.primaryColor};
+  color: ${(prop) => prop.theme.black};
+
+  &:hover {
+    background-color: #fc3d21;
+    color: ${(prop) => prop.theme.white};
+  }
+`;
+
+//JSX
+
 function NasaPhoto(props) {
   const { url, date, explanation, mediaType, title } = props;
 
   return (
     <>
-      <div className="apod-wrapper">
-        <div className="header">
-          <h1 className="apod-title">NASA APOD</h1>
+      <ApodWrapperDiv>
+        <HeaderDiv media={mediaType}>
+          <ApodTitleH1>NASA APOD</ApodTitleH1>
           <h2>Astronomy Picture of the Day</h2>
-        </div>
-        <div className="main-content">
+        </HeaderDiv>
+        <MainContentDiv>
           <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png" alt="NASA Logo" />
-          <div className="text-wrapper">
+          <TextWrapperDiv>
             <h2>{title}</h2>
             <p>{date}</p>
             <p>{explanation}</p>
-          </div>
-          <button onClick={modal} className="button">
-            Click for a Full Screen View
-          </button>
+          </TextWrapperDiv>
+          <Button onClick={modal}>Click for a Full Screen View</Button>
           <div className="nasa-pic-wrapper">
             {mediaType === "video" ? (
               <iframe src={url} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={title} />
@@ -38,14 +97,14 @@ function NasaPhoto(props) {
               <img src={url} alt="APOD Image" />
             )}
           </div>
-        </div>
-      </div>
+        </MainContentDiv>
+      </ApodWrapperDiv>
       <div className="modal">
         <div className="nasa-pic-wrapper">
           {mediaType === "video" ? (
             <iframe src={url} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={title} />
           ) : (
-            <img src={url} alt="APOD Image" />
+            <img src={url} alt="APOD" />
           )}
         </div>
       </div>
---
+++ src/theme/theme.js
@@ -0,0 +1,7 @@
+export default {
+  primaryColor: "#d3d3df",
+  secondaryColor: "#0b3d91",
+  black: "black",
+  white: "white",
+  breakpointMobile: "(max-width: 550px)",
+};
